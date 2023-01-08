69ff0a5f4f329938b7c817cc8a73faa2dd558afc
Oleksandr <olek.olefirenko7@gmail.com>
2023-01-08 19:32:54 +0100

    cambios html y js
    cambios html y js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ document.getElementById("formulario").addEventListener("submit", function(event)
     }
 }
 );
+
+$("#tam").on('change', function() {
+    let tamm = document.getElementById("resultado_tamano");
+    console.log(this.value);
+    
+    fetch("http://localhost:5000/checksize", {
+        method: "post"
+    }). then(function(response) {
+        tamm.innerHTML = response.text();
+    }).catch(function(err) {
+        console.log(err);
+    });
+});
